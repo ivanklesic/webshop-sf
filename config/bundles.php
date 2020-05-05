d6b53974a6ade13ef21d8c9e761957d48dc77570
ivanklesic <ivan.klesic@gmail.com>
2020-05-05 16:14:46 +0200

    installed neo4j and OGM

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Neo4j\Neo4jBundle\Neo4jBundle::class => ['all' => true],
 ];
